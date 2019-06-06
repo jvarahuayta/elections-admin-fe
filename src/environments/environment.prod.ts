131bfe5eb5d56b3679a87b9d6ebc91506afbd037
jvarahuayta <jvarahuayta@gmail.com>
2019-06-06 16:27:24 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDClfbM4skR5qLOfFaIuQexRbWA5sQ5kA4',
+    authDomain: 'fitco-demo.firebaseapp.com',
+    databaseURL: 'https://fitco-demo.firebaseio.com',
+    projectId: 'fitco-demo',
+    storageBucket: 'fitco-demo.appspot.com',
+    messagingSenderId: '26406224578'
+  }
 };
